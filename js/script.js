778328864f349d7f02a076698b2fe0ea5f6a7b95
Przemysław Bulanda <devprzemas@gmail.com>
2023-10-09 15:52:50 +0200

    Add rest code in titeClickHandler

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
     console.log('links:', links);
   }); */
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event', event);
@@ -27,11 +28,18 @@ const titleClickHandler = function(event){
     for(let activeArticle of activeArticles){
        activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+    
+    
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
     const links = document.querySelectorAll('.titles a');
     for(let link of links){
